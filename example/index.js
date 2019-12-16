eeb38a7e38c4556f84c31b0bd8cfd766db480156
czewail <chanzewail@gmail.com>
2019-12-16 11:11:10 +0800

    更新example

--- example/index.js
+++ example/index.js
@@ -2,16 +2,19 @@ import * as React from '../dist'
 
 class Example extends React.Component {
   render() {
-    return <div>
+    return <div className="container2">
       hello zewail
     </div>
   }
 }
 
-function Example2 () {
-  return <div>
+function Example2 (props) {
+  return <div className={props.className} style={props.style}>
     hello zewail2
     </div>
 }
 
-React.render(<Example2 />, document.getElementById('app'))
+React.render(<div>
+  <Example />
+  <Example2 className="container" style={{ fontSize: '18px' }} />
+</div>, document.getElementById('app'))
